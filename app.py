459636e4ac341a791da663a9748177e544557ed0
Saranga Kumarapeli <65158515+Saranga99@users.noreply.github.com>
2022-12-26 16:24:11 +0530

    update

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import time
+import keras_cv
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+
+model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+
+images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=1)
+
+
+def plot_images(images):
+    plt.figure(figsize=(20, 20))
+    for i in range(len(images)):
+        ax = plt.subplot(1, len(images), i + 1)
+        plt.imshow(images[i])
+        plt.axis("off")
+
+
+plot_images(images)
